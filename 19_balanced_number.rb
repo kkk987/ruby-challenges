c699412511669e1910d03604f392d2493f1927a8
kkk987 <markying21549@gmail.com>
2019-05-07 10:09:41 +1000

    19 balanced number done

--- 19_balanced_number.rb
+++ 19_balanced_number.rb
@@ -47,6 +47,62 @@
 # and the sum of all digits to the right of the middle digits (20) are equal, then it's not balanced.
 # Note : The middle digit(s) are 02.
 
+def cal_left(number, index)
+    i = 0
+    sum = 0
+    while i < index
+        # puts "Number in left #{number[i].to_i}"
+        sum += number[i].to_i
+        i += 1
+    end
+    return sum
+end
+
+def cal_right(number, index)
+    sum = 0
+    while index < number.length
+        # puts "Number in right #{number[index].to_i}"
+        sum += number[index].to_i
+        index += 1
+    end
+    return sum
+end
+
+def compare_sum(left, right)
+    if left == right
+        return "Balanced"
+    else
+        return "Not balanced"
+    end
+end
+
 def balanced_num(number)
     # Your code goes here
+    # puts "number #{number}"
+    length = number.to_s.length
+    # puts "Length #{length}"
+    m_index = length/2
+    # puts "m_index #{m_index}"
+    num_str = number.to_s
+    if length % 2 == 1
+        middle_digit = num_str[m_index].to_i
+        left_sum = cal_left(num_str, m_index)
+        right_sum = cal_right(num_str, m_index + 1)
+        return compare_sum(left_sum, right_sum)
+    else
+        middle_digit = num_str[m_index - 1].to_i * 10 + num_str[m_index].to_i
+        # puts "middile digits #{middle_digit}"
+        left_sum = cal_left(num_str, m_index - 1)
+        right_sum = cal_right(num_str, m_index + 1)
+        return compare_sum(left_sum, right_sum)
+    end
 end
+
+#Expect balanced
+puts balanced_num(7)
+#Expect not balanced
+puts balanced_num(295591)
+#Expect balanced
+puts balanced_num(959)
+#Expect not balanced
+puts balanced_num(27102983)
